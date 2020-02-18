a317888ff2988e19e7c6dc27cb3de063cfbe414c
Ricardo Miranda <ricardo.miranda@deimos.com.pt>
2020-02-18 11:29:42 +0000

    PGFMWS-66: Julia corrected a bug that prevented the code from running

--- algorithms/LsmDemFlagging.jl
+++ algorithms/LsmDemFlagging.jl
@@ -1,33 +1,23 @@
-output = [:(x), :(y), :(z)]
-@generated function outputFun(x, y, z)
-    :($(output[1]), $(output[2]), $(output[3]))
-end
-
+const scaleFactorLsm = 10^(-4)
+const scaleFactorDem = 1
 include("../utilities/BitwiseOperator.jl")
 include("../utilities/HaversineDistance.jl")
 include("../utilities/computeLongitudes.jl")
 include("../utilities/getGridSequencePos.jl")
 
-function lsm_dem_flagging(n_fovs::Int,resol,
-    flag_lsm,n_lat_lsm,n_lon_inlat_lsm,n_gridpoints_lsm,lat1_lsm,lon1_lsm,land_sea_mask,
-    flag_dem,n_lat_dem,n_lon_inlat_dem,n_gridpoints_dem,lat1_dem,lon1_dem, digital_elevation_model,
-    l0_nscans::Int, pos_lat::Array{Float64, 2}, pos_lon::Array{Float64, 2}, nav_status)
-
+function lsm_dem_flagging(n_fovs,resol,flag_lsm,n_lat_lsm,n_lon_inlat_lsm,n_gridpoints_lsm,lat1_lsm,lon1_lsm,land_sea_mask,flag_dem,n_lat_dem,n_lon_inlat_dem,n_gridpoints_dem,lat1_dem,lon1_dem, digital_elevation_model, l0_nscans, pos_lat, pos_lon, nav_status)
     flag_surface = zeros(Int8, n_fovs, l0_nscans)
-    for i in 1:l0_nscans
-        flag_surface[i, 1] = copyBitInto(nav_status[1], 0, flag_surface[i,1], 1)
-        for j in 1:n_fovs
-            flag_surface[i, j] = copyBitInto(flag_lsm, 0, flag_surface[i,j], 2)
-            flag_surface[i, j] = copyBitInto(flag_dem, 0, flag_surface[i,j], 3)
+    surface_type = zeros(Float64, length(resol), n_fovs, l0_nscans)
+    terrain_elevation = zeros(Float64, length(resol), n_fovs, l0_nscans)
+    for i in 1:n_fovs
+        flag_surface[i, 1] = copyBitInto(convert(Int64,nav_status[1]), 0, convert(Int64,flag_surface[i,1]), 1)
+        for j in 1:l0_nscans
+            flag_surface[i, j] = copyBitInto(convert(Int64,flag_lsm), 0, convert(Int64,flag_surface[i,j]), 2)
+            flag_surface[i, j] = copyBitInto(convert(Int64,flag_dem), 0, convert(Int64,flag_surface[i,j]), 3)
         end
     end
-
     dict_lsm = computeLongitudes(n_lon_inlat_lsm, lon1_lsm, lat1_lsm)
     dict_dem = computeLongitudes(n_lon_inlat_dem, lon1_dem, lat1_dem)
-
-    surface_type = Array{Float64, 3}(undef, length(resol), n_fovs, l0_nscans)
-    terrain_elevation = Array{Float64, 3}(undef, length(resol), n_fovs, l0_nscans)
-
     if flag_lsm % 2 == 0 && flag_dem % 2 == 0 && n_lon_inlat_dem == n_lon_inlat_lsm
         for i in 1:l0_nscans
             if nav_status[i] % 2 == 0
@@ -39,19 +29,28 @@ function lsm_dem_flagging(n_fovs::Int,resol,
                             for lon in dict_lsm[lat]
                                 if haversineDist(lat, lon, pos_lat[i,j], pos_lon[i,j]) < resol[k]
                                     counter+=1
-                                    surface_type[i,j,k] = land_sea_mask[getGridSequencePos(lat1_lsm, n_lon_inlat_lsm, dict_lsm, lat, lon)]
-                                    terrain_elevation[i,j,k] = digital_elevation_model[getGridSequencePos(lat1_dem, n_lon_inlat_dem, dict_dem, lat, lon)]
+                                    surface_type[k,j,i] += land_sea_mask[getGridSequencePos(lat1_lsm, n_lon_inlat_lsm, dict_lsm, lat, lon)]
+                                    terrain_elevation[k,j,i] += digital_elevation_model[getGridSequencePos(lat1_dem, n_lon_inlat_dem, dict_dem, lat, lon)]
                                 end
                             end
                         end
-                        surface_type[i,j,k] = round(surface_type[i,j,k] / counter, digits=4)
-                        terrain_elevation[i,j,k] = round(terrain_elevation[i,j,k] / counter, digits=4)
+                        surface_type[k,j,i] = round(scaleFactorLsm * (surface_type[k,j,i] / counter), RoundNearest)
+                        terrain_elevation[k,j,i] = round(scaleFactorDem * (terrain_elevation[k,j,i] / counter),RoundNearest)
                     end
                 end
             end
         end
     end
-
-
     return outputFun(flag_surface, surface_type, terrain_elevation)
+    #output = Dict{String, AbstractArray}()
+    #push!(output, "flag_surface" => flag_surface)
+    #push!(output, "surface_type" => surface_type)
+    #push!(output, "terrain_elevation" => terrain_elevation)
+    #return output
+end
+
+
+output = [:(x), :(y), :(z)]
+@generated function outputFun(x, y, z)
+    :($(output[1]), $(output[2]), $(output[3]))
 end
--- utilities/BitwiseOperator.jl
+++ utilities/BitwiseOperator.jl
@@ -1,11 +1,7 @@
 # Copies the bit in bitpos_source from the source into the bit in position bitpos_target of the target
-function copyBitInto(source::Int, bitpos_source::Int64, target::Int8, bitpos_target::Int64)
-        result = 0
-        oneMask = 2^bitpos_source
-        result = (((source & oneMask) >> bitpos_source) << bitpos_target) | target
-        return convert(typeof(target), result)
-end
-
-function copyBitInto(source::Vector{Int}, pos_source::Int64, target::Vector{Int8}, pos_target::Int64)
-
+function copyBitInto(source, bitpos_source, target, bitpos_target)
+    bitRes = 0
+    oneMask = 2^bitpos_source
+    bitRes = (((source & oneMask) >> bitpos_source) << bitpos_target) | target
+    return convert(typeof(target), bitRes)
 end
